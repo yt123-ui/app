3dfc07db095ecdf43858e019f8f060f957060218
yt123-ui <1060167494@qq.com>
2020-04-08 16:17:58 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/styles/reset.css';
+import './assets/fonts/iconfont.css';
+import App from './App';
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
